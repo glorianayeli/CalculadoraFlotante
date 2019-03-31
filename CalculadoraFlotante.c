8143f76d929071f2e874ac5138a9ff08c67f99bf
gloria nayeli monreal jaime <glorianayeli_jaime@ucol.mx>
2019-03-31 15:58:20 -0600

    correccion salida serial de datos

--- CalculadoraFlotante.c
+++ CalculadoraFlotante.c
@@ -8,7 +8,7 @@
 #ifdef __DEBUG_SERIAL__
    #define TX_232        PIN_C6
    #define RX_232        PIN_C7
-   #use RS232(BAUD=9600, XMIT=TX_232, BITS=8,PARITY=N, STOP=1)
+   #use RS232(BAUD=9600, XMIT=TX_232, BITS=8,PARITY=N, STOP=1,UART1,RCV=RX_232)
    #use fast_io(c)
 #endif 
 
